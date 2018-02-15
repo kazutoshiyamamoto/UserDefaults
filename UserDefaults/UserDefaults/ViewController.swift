d45c170a25a657119231edbefef1292816a9fa34
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-02-15 23:52:48 +0900

    スイッチをOutlet接続

---
+++ UserDefaults/UserDefaults/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  UserDefaults
+//
+//  Created by home on 2018/02/15.
+//  Copyright © 2018年 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // スイッチをOutlet接続する
+    @IBOutlet weak var mySwitch1: UISwitch!
+    @IBOutlet weak var mySwitch2: UISwitch!
+    
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
