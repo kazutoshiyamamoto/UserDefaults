4e3446969961d5ebc86c65353c47e3582662301a
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-02-16 00:27:54 +0900

    保存/読み込み

--- UserDefaults/UserDefaults/ViewController.swift
+++ UserDefaults/UserDefaults/ViewController.swift
@@ -14,8 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var mySwitch1: UISwitch!
     @IBOutlet weak var mySwitch2: UISwitch!
     
-
+    // ユーザーデフォルトに保存する
+    @IBAction func saveStatus(_ sender: Any) {
+    // ユーザーデフォルトを参照する
+    let defaults = UserDefaults.standard
+    // mySwitch1の値をキー”switchOn"で保存する
+        defaults.set(mySwitch1.isOn, forKey: "switchOn")
+    }
     
+    // ユーザーデフォルトから読み込む
+    @IBAction func readStatus(_ sender: Any) {
+        // ユーザーデフォルトを参照する
+        let defaults = UserDefaults.standard
+        // キー”switchOn"の値をBoolとして読んでmySwitch2に設定する
+        mySwitch2.isOn = defaults.bool(forKey: "switchOn")
+    }
     
     
     override func viewDidLoad() {
